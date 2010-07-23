9ca735d8bd1bea64cf819ab6a39a867f4c4d11ec
John Bytheway <52664+jbytheway@users.noreply.github.com>
2010-07-23 23:08:40 +0100

    Combine getter_setter with access to glEnable et al
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- cagoul/cleardepth.hpp
+++ cagoul/cleardepth.hpp
@@ -1,11 +1,11 @@
 #ifndef CAGOUL__CLEARDEPTH_HPP
 #define CAGOUL__CLEARDEPTH_HPP
 
-#include <cagoul/getter_setter.hpp>
+#include <cagoul/getter_setter_toggler.hpp>
 
 namespace cagoul {
 
-static const getter_setter<
+static const getter_setter_toggler<
   double, 1,
   glClearDepth,
   GL_DEPTH_CLEAR_VALUE
--- cagoul/depthfunc.hpp
+++ cagoul/depthfunc.hpp
@@ -2,14 +2,15 @@
 #define CAGOUL__DEPTHFUNC_HPP
 
 #include <cagoul/enums/depthfunc.hpp>
-#include <cagoul/getter_setter.hpp>
+#include <cagoul/getter_setter_toggler.hpp>
 
 namespace cagoul {
 
-static const getter_setter<
+static const getter_setter_toggler<
   enums::DepthFunc, 1,
   glDepthFunc,
-  GL_DEPTH_FUNC
+  GL_DEPTH_FUNC,
+  GL_DEPTH_TEST
 > DepthFunc;
 
 }
